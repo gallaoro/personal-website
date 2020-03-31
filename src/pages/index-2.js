a82f17af353036acde1de356ef64d2354b0e8ecf
Gabriele <gabriele.pallaoro@advisoreat.com>
2020-03-31 19:08:34 +0200

    fix the fix

--- src/pages/index-2.js
+++ src/pages/index-2.js
@@ -24,13 +24,13 @@ class IndexPage extends React.Component {
           </div>
           <div className="lg:w-2/3 md:w-3/4 w-full flex justify-center items-start sm:items-center p-10">
             <div className="bg-white rounded-lg">
-              <div className="w-auto inline-block p-8">
+              <div className="w-auto inline-block pt-8">
                 <div className="flex items-center">
                   <div className="rounded-full w-20 h-20">
                     <img className="rounded-full shadow-inner" alt="me" style={{ filter: "grayscale(20%)" }} src="https://pbs.twimg.com/profile_images/1194354546824400898/l2jMkcO__400x400.jpg" />
                   </div>
                   <div className="pl-6 flex items-center">
-                    <h1 className="font-sans text-2xl sm:text-3xl font-bold text-gray-800 mb-2">Gabriele Pallaoro</h1>
+                    <h1 className="font-sans text-3xl font-bold text-gray-800 mb-2">Gabriele Pallaoro</h1>
                   </div>
                 </div>
                 <div className="pt-4">
