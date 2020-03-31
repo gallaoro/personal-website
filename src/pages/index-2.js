a4064ffd920a29ea6d915bddf4866ff97f671d9c
Gabriele <gabriele.pallaoro@advisoreat.com>
2020-03-31 19:27:17 +0200

    tuning mobile

--- src/pages/index-2.js
+++ src/pages/index-2.js
@@ -6,31 +6,23 @@ import "../css/global.css"
 import "../css/background.css"
 class IndexPage extends React.Component {
 
-  componentDidMount() {
-    // fix for the viewport height in mobile browsers
-    window.addEventListener('resize', () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-  }
-
   render() {
     return (
       <>
         <SEO title="Home" />
         {/* eslint-disable-next-line no-dupe-keys */}
-        <div className="flex" style={{ height: '100vh', height: 'calc(var(--vh, 1vh) * 100)' }} id="main-section">
+        <div className="flex h-screen" id="main-section">
           <div className="lg:w-1/3 md:w-1/4 w-0">
           </div>
           <div className="lg:w-2/3 md:w-3/4 w-full flex justify-center items-start sm:items-center pt-10 px-6">
             <div className="bg-white rounded-lg">
-              <div className="w-auto inline-block p-8">
+              <div className="w-auto inline-block p-6 sm:p-8">
                 <div className="flex items-center">
                   <div className="rounded-full w-20 h-20 flex items-center">
                     <img className="rounded-full shadow-inner" alt="me" style={{ filter: "grayscale(20%)" }} src="https://pbs.twimg.com/profile_images/1194354546824400898/l2jMkcO__400x400.jpg" />
                   </div>
                   <div className="pl-6 flex items-center">
-                    <h1 className="font-sans text-2xl sm:text-3xl font-bold text-gray-800 mb-2">Gabriele Pallaoro</h1>
+                    <h1 className="font-sans text-2xl sm:text-3xl font-bold text-gray-800">Gabriele Pallaoro</h1>
                   </div>
                 </div>
                 <div className="pt-4">
