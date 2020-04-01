3e3762ae63683c217a60d911ea68bedf6d6e6270
Gabriele <gabriele.pallaoro@advisoreat.com>
2020-04-01 11:48:14 +0200

    fix 404 padding

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,16 +8,20 @@ import "../css/global.css"
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <div className="container mx-auto">
-      <div className="rounded mt-16">
-        <h1 className="font-sans text-red-600 text-lg"><b>404!</b> This is not the page you are looking for</h1>
-      </div>
-      <div>
-        <Link to="/" title="homepage">
-          <div className="pt-6">
-            <p className="font-sans">← go to the homepage <span role="img" aria-label="home">🏠</span></p>
-          </div>
-        </Link>
+    <div>
+      <div class="container mx-auto px-6">
+        <div class="pt-16">
+          <h1 class="font-sans text-red-600 text-lg">
+            <b>404!</b> This is not the page you are looking for
+          </h1>
+        </div>
+        <div class="mt-6 border rounded inline-block bg-white">
+          <Link title="homepage" to="/">
+            <div class="py-2 px-4">
+              <p class="font-sans">← go to the homepage <span role="img" aria-label="home">🏠</span></p>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   </>
