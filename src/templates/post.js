340f7c67bb3884eb1dde9da76cc682e63e797fc6
Gabriele <gabriele.pallaoro@advisoreat.com>
2020-04-02 22:19:21 +0200

    update post style

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import "../css/alternate_background.css"
+
 export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="pt-16" >
-      <div className="container mx-auto">
+    <div className="pt-16 h-screen" id="main-section-alternate">
+      <div className="container mx-auto px-6">
         <h1 className="text-3xl font-bold text-black">{frontmatter.title}</h1>
         <h2 className="text-xl text-gray-700 pb-16">{frontmatter.date}</h2>
         <div
